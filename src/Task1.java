666cad0147f0cb51a66249bcf6b79659cb25cc56
Людок <skobeleva.ludmila@yandex.ru>
2022-12-27 23:46:09 +0300

    Первый коммит

---
+++ src/Task1.java
@@ -0,0 +1,13 @@
+import java.time.LocalDateTime;
+
+public class Task1 {
+    private String titel;
+    private String description;
+    private Type type;
+    private LocalDateTime dateTime;
+
+
+
+
+}
+
